756d16b5229a52ac6b227b80466d24f80048453f
Sam Hogue <eugohmas@gmail.com>
2023-09-06 10:04:28 -0600

    logger update

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const path = require('path');
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+//init middleware
+app.use(logger);
+
 //set a static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+//We can do this, but if we do this, we have to create a route manually for every single page with the /path
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 5000;
 
